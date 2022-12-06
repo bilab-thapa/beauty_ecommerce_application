507d58aeedcf0dc1ac578fde78f01cf7346bbda0
AyushJungKC7 <ayushjungk@gmail.com>
2022-12-06 08:23:03 +0545

    data models

---
+++ lib/models/shipping_model.dart
@@ -0,0 +1,17 @@
+class Shipping {
+  String? firstName;
+  String? lastName;
+  String? city;
+  String? address;
+  String? number;
+  String? status;
+
+  Shipping({
+    required this.firstName,
+    required this.lastName,
+    required this.city,
+    required this.address,
+    required this.number,
+    required this.status,
+  });
+}
