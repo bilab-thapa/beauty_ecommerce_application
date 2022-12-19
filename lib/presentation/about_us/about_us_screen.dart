0ba619398cb8df179fa559f7ec9d5543d5e120e6
junagc <junagc143@gmail.com>
2022-12-19 08:58:19 +0545

    About Us Section

---
+++ lib/presentation/about_us/about_us_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+class AboutUsPage extends StatefulWidget {
+  @override
+  _AboutUsPageState createState() => _AboutUsPageState();
+}
+
+class _AboutUsPageState extends State<AboutUsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'About Us',
+          style: GoogleFonts.comfortaa(color: Colors.white, fontSize: 28),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              'Welcome to our About Us page!',
+              style: GoogleFonts.comfortaa(color: Colors.black, fontSize: 20),
+            ),
+            SizedBox(
+              child: Lottie.network(
+                  'https://assets8.lottiefiles.com/packages/lf20_lstcbn9k.json'),
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+                'At Beauty, we believe that beauty should be accessible to everyone. That\'s why we have developed a range of high-quality, affordable cosmetics that cater to a variety of skin tones, skin types, and makeup preferences. Our products are formulated with nourishing, natural ingredients and are never tested on animals. We are committed to providing our customers with exceptional products and service, and we strive to continuously improve and expand our offerings. Thank you for choosing Beauty and for supporting our mission to make beauty accessible for all.',
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.comfortaa()),
+            const SizedBox(height: 30.0),
+            Text(
+              'Thank you for using our app!',
+              style: GoogleFonts.comfortaa(color: Colors.black, fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
